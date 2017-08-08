8545e3f37b6fe79fb2b57571f4e562f568d4a815
cpc-coder <1914701068@qq.com>
2017-08-08 15:46:28 +0800

    bug

--- demo/TransionTool/CPCTransionAnim.swift
+++ demo/TransionTool/CPCTransionAnim.swift
@@ -13,6 +13,25 @@ class CPCTransionAnim: CPCBaseVCTransition{
     fileprivate var transitionContext : UIViewControllerContextTransitioning!
     
     
+    fileprivate var toViewC:UIViewController?
+    
+    override func config(container: UIView, fromView: UIView, toView: UIView, initalFrame: CGRect, transitionContext: UIViewControllerContextTransitioning) -> (UIView?, UIView?) {
+        
+        
+        
+        
+        if operation == .push {
+            //去除tabbar动画
+            let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)! as UIViewController
+            fromVC.tabBarController?.tabBar.isHidden = true
+        } else {
+            //去除tabbar动画
+            let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)! as UIViewController
+            toVC.tabBarController?.tabBar.isHidden = true
+        }
+        return (fromView,toView)
+    }
+    
     override func animation(container: UIView, fromView: UIView?, toView: UIView?, transitionContext: UIViewControllerContextTransitioning) {
         
         
@@ -24,12 +43,24 @@ class CPCTransionAnim: CPCBaseVCTransition{
         anim.isRemovedOnCompletion = true
         anim.delegate = self
         container.layer.add(anim, forKey: nil)
+        
+        
+        //去除tabbar动画
+        toViewC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)! as UIViewController
+        
+
     }
    
     
 }
 extension CPCTransionAnim:CAAnimationDelegate {
     func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        
+        
+        if operation == .pop
+        {
+            toViewC?.tabBarController?.tabBar.isHidden = false
+        }
         
         transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
     }
